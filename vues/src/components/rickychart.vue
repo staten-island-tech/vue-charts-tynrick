5c018253608d383505be21fde13c5a843ff3b5a4
RickyLi11228 <124058517+RickyLi11228@users.noreply.github.com>
2024-03-20 21:33:04 -0400

    supposed ot work but no graph

--- vues/src/components/rickychart.vue
+++ vues/src/components/rickychart.vue
@@ -4,29 +4,26 @@
   </div>
 </template>
 
-
 <script>
 import { Bar } from 'vue-chartjs'
-import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, LinearScale } from 'chart.js'
 
-
-ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
-
+ChartJS.register(Title, Tooltip, Legend, BarElement, LinearScale)
 
 export default {
   name: 'BarChart',
   components: { Bar },
-  data: () => ({
-    loaded: false,
-    chartData: null
-  }),
-  async mounted () {
+  data() {
+    return {
+      loaded: false,
+      chartData: null
+    }
+  },
+  async mounted() {
     this.loaded = false
-
-
     try {
       const response = await fetch('https://data.cityofnewyork.us/resource/jb7j-dtam.json')
-      if (response.status !=200) {
+      if (response.status !== 200) {
         throw new Error(response.statusText)
       }
       const data = await response.json();
@@ -34,22 +31,19 @@ export default {
       const labels = [];
       const datasets = [];
 
-
       data.forEach(item => {
         labels.push(item.year);
         datasets.push({
-          ethnicity: item.race_ethnicity,
-           data: [item.deaths],
+          label: item.race_ethnicity,
           backgroundColor: '#f87979',
+          data: item.deaths,
         });
       });
 
-
       this.chartData = {
-        labelss: labels,
-        datasetss: datasets,
+        labels: labels,
+        datasets: datasets,
       };
-
 
       this.loaded = true;
     } catch (error) {
