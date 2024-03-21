5f9859a2e81521935d7d2ee7faa12204248b1ae7
RickyLi11228 <124058517+RickyLi11228@users.noreply.github.com>
2024-03-21 09:59:16 -0400

    api works graph works! but broken label

--- vues/src/components/rickychart.vue
+++ vues/src/components/rickychart.vue
@@ -4,46 +4,58 @@
   </div>
 </template>
 
+
 <script>
 import { Bar } from 'vue-chartjs'
-import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, LinearScale } from 'chart.js'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
 
-ChartJS.register(Title, Tooltip, Legend, BarElement, LinearScale)
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
 
 export default {
   name: 'BarChart',
   components: { Bar },
-  data() {
-    return {
-      loaded: false,
-      chartData: null
-    }
-  },
-  async mounted() {
+  data: () => ({
+    loaded: false,
+    chartData: null
+  }),
+  async mounted () {
     this.loaded = false
+
+
     try {
       const response = await fetch('https://data.cityofnewyork.us/resource/jb7j-dtam.json')
-      if (response.status !== 200) {
+      if (response.status !=200) {
         throw new Error(response.statusText)
       }
       const data = await response.json();
-     
+      const pee = [];
       const labels = [];
       const datasets = [];
 
+
       data.forEach(item => {
         labels.push(item.year);
-        datasets.push({
+        pee.push(item.race_ethnicity)
+        if (pee.includes(item.race_ethnicity)){
+          console.log(pee)
+        } else{
+          data.push({
           label: item.race_ethnicity,
-          backgroundColor: '#f87979',
           data: item.deaths,
+          backgroundColor: '#f87979',
         });
+        }
+
       });
+
 
       this.chartData = {
         labels: labels,
         datasets: datasets,
       };
+
 
       this.loaded = true;
     } catch (error) {
