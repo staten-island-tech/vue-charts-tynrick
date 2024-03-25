9121586074fd8fb49262e65e294a8bc3383cfe39
harry38444 <harry38444@gmail.com>
2024-03-25 10:13:15 -0400

    donut chart
    doughnut no work

--- vues/src/components/harrychart2.vue
+++ vues/src/components/harrychart2.vue
@@ -1,5 +1,5 @@
 <template>
-    <Bar
+    <Bar v-if = "loaded"
       id="my-chart-id-2"
       :options="chartOptions"
       :data="chartData"
@@ -7,24 +7,67 @@
   </template>
   
   <script>
-  import { Bar } from 'vue-chartjs'
-  import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
+import { Doughnut } from 'vue-chartjs'
   
-  ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
-  
-  export default {
-    name: 'BarChart2',
-    components: { Bar },
-    data() {
-      return {
-        chartData: {
-          labels: [ 'January', 'Apirl', 'May' ],
-          datasets: [ { data: [40, 50, 32] } ]
-        },
-        chartOptions: {
-          responsive: true
-        }
+ChartJS.register(ArcElement, Tooltip, Legend)
+
+export default {
+  name: 'App',
+  components: {
+    Doughnut
+  },
+  data: () => ({
+    loaded: false,
+    chartData: null
+  }),
+  async mounted () {
+    this.loaded = false
+
+
+    try {
+      const response = await fetch('https://data.cityofnewyork.us/resource/jb7j-dtam.json')
+      if (response.status !=200) {
+        throw new Error(response.statusText)
       }
+      const data = await response.json();
+      const race = [];
+      const year = [];
+      const labels = [];
+      const datasets = [];
+
+
+      data.forEach(item => {
+        if (year.includes(item.year)){
+          console.log(datasets)
+        } else{
+        labels.push(item.year)
+        }
+        year.push(item.year)
+        if (race.includes(item.race_ethnicity)){
+          console.log(datasets)
+        } else{
+          datasets.push({
+          label: item.race_ethnicity,
+          data: item.deaths,
+          backgroundColor: '#f87979',
+        });
+        }
+        race.push(item.race_ethnicity)
+
+      });
+
+
+      this.chartData = {
+        labels: labels,
+        datasets: datasets,
+      };
+
+
+      this.loaded = true;
+    } catch (error) {
+      console.error('oopsies', error)
     }
   }
-  </script>
+}
+</script>
