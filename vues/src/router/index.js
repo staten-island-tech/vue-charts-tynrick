ea53aa2138bed103746315d99ca24f15664b019e
harry38444 <harry38444@gmail.com>
2024-04-02 00:22:04 -0400

    broo
    adafaw

--- vues/src/router/index.js
+++ vues/src/router/index.js
@@ -1,16 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import LineView from '../views/LineView.vue'
+import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'line',
-      component: LineView
+      name: 'home',
+      component: HomeView
     },
     
-  
+
     {
       path: '/pie',
       name: 'pie',
