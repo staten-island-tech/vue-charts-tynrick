d26b030f0e530f085f8aa68279095eecc754827d
RickyLi11228 <124058517+RickyLi11228@users.noreply.github.com>
2024-04-01 22:34:33 -0400

    a

--- vues/src/App.vue
+++ vues/src/App.vue
@@ -14,6 +14,7 @@ import { RouterLink, RouterView } from 'vue-router'
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
         <RouterLink to="/line">Line Chart</RouterLink>
+        <RouterLink to="/pie">Pie Chart</RouterLink>
       </nav>
     </div>
   </header>
